9604537cc68001061998ca8ade1a5776064a0e69
tino <tino@tino.tino>
2024-12-07 02:45:47 +0100

    (feta #50) added module - route- component

---
+++ src/app/features/culinary-alchemy/culinary-alchemy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-culinary-alchemy',
+  standalone: true,
+  imports: [],
+  templateUrl: './culinary-alchemy.component.html',
+  styleUrl: './culinary-alchemy.component.css'
+})
+export class CulinaryAlchemyComponent {
+
+}
