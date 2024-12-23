0d063d395266c996a25a010132babbe1be00a3b5
tino <tino@tino.tino>
2024-12-23 02:09:48 +0100

    added code for info header meal master

--- src/app/features/meal-master/meal-master.module.ts
+++ src/app/features/meal-master/meal-master.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MealMasterRoutingModule } from './meal-master-routing.module';
+import { InfoHeaderMealMasterComponent } from './info-header-meal-master/info-header-meal-master.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MealMasterRoutingModule
+    MealMasterRoutingModule,
+    InfoHeaderMealMasterComponent
   ]
 })
 export class MealMasterModule { }
