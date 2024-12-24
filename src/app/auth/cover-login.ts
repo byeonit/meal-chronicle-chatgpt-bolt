ff733805dae0dabfef5da9c175acce9ff735b01e
tino <tino@tino.tino>
2024-12-24 12:30:16 +0100

    added code for signup as Guest inside cover-login

--- src/app/auth/cover-login.ts
+++ src/app/auth/cover-login.ts
@@ -7,56 +7,70 @@ import { AppService } from 'src/app/service/app.service';
 import { AuthService } from './services/firebase/auth.service';
 
 @Component({
-    templateUrl: './cover-login.html',
-    animations: [toggleAnimation],
+  templateUrl: './cover-login.html',
+  animations: [toggleAnimation],
 })
 export class CoverLoginComponent {
-    public email = '';
-    public password = '';
-  
-    store: any;
-    currYear: number = new Date().getFullYear();
-    constructor(
-        public translate: TranslateService,
-        public storeData: Store<any>,
-        public router: Router,
-        private appSetting: AppService,
-        private authService: AuthService,
-    ) {
-        this.initStore();
-    }
-    async initStore() {
-        this.storeData
-            .select((d) => d.index)
-            .subscribe((d) => {
-                this.store = d;
-            });
-    }
+  public email = '';
+  public password = '';
+  error: string | null = null;
 
-    changeLanguage(item: any) {
-        this.translate.use(item.code);
-        this.appSetting.toggleLanguage(item);
-        if (this.store.locale?.toLowerCase() === 'ae') {
-            this.storeData.dispatch({ type: 'toggleRTL', payload: 'rtl' });
-        } else {
-            this.storeData.dispatch({ type: 'toggleRTL', payload: 'ltr' });
-        }
-        window.location.reload();
-    }
+  store: any;
+  currYear: number = new Date().getFullYear();
+  constructor(
+    public translate: TranslateService,
+    public storeData: Store<any>,
+    public router: Router,
+    private appSetting: AppService,
+    private authService: AuthService,
+  ) {
+    this.initStore();
+  }
+  async initStore() {
+    this.storeData
+      .select((d) => d.index)
+      .subscribe((d) => {
+        this.store = d;
+      });
+  }
 
-    async login() {
-        try {
-          const user = await this.authService.login(this.email, this.password);
-          if (user) {
-            //this.error = null;
-            console.log('User logged in:', user);
-    
-            // Redirect to dashboard
-            this.router.navigate(['/']);
-          }
-        } catch (error: any) {
-          console.log('User error :', error);
-          //this.error = error.message;
-        }
+  changeLanguage(item: any) {
+    this.translate.use(item.code);
+    this.appSetting.toggleLanguage(item);
+    if (this.store.locale?.toLowerCase() === 'ae') {
+      this.storeData.dispatch({ type: 'toggleRTL', payload: 'rtl' });
+    } else {
+      this.storeData.dispatch({ type: 'toggleRTL', payload: 'ltr' });
+    }
+    window.location.reload();
+  }
+
+  async login() {
+    try {
+      const user = await this.authService.login(this.email, this.password);
+      if (user) {
+        //this.error = null;
+        console.log('User logged in:', user);
+
+        // Redirect to dashboard
+        this.router.navigate(['/']);
       }
+    } catch (error: any) {
+      console.log('User error :', error);
+      //this.error = error.message;
+    }
+  }
+
+  async guestLogin() {
+    try {
+      const user = await this.authService.signInAnonymously();
+
+      console.log('Guest logged in:', user);
+
+      this.router.navigate(['/']);
+    } catch (error: any) {
+      console.log('Guest Login error :', error);
+      this.error = error.message;
+    }
+  }
 }
